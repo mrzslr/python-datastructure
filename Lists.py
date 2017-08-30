db5874a52125af43109d844e6274dcb09c038ec2
MohammadReza <frishter.ms@gmail.com>
2017-08-30 13:20:24 +0430

    initilize commit, List & Stack with array.

---
+++ Lists.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# In[9]:
+
+
+# b=[24,25,26]
+# a.append(b)
+# a.extend(b)
+a
+
+
+# In[5]:
+
+
+a=[23,25,-100,1024,512]
+a.append(256)
+a.insert(0,1)
+a.reverse()
+a
+
---
+++ Stack.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+stack=[1,2,3,4,5,6,7,8]
+stack.append(9)
+stack.append(10)
+stack.pop()
+print(stack)
+
